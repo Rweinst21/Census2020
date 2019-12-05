6388a7c83488756ffd2e581ab212854f6c27b44d
jennal-datacenter <42979143+jennal-datacenter@users.noreply.github.com>
2019-12-05 10:35:43 -0600

    adding city and metro response, plus reorged clean-up code a bit (which modified the csvs) - nothing substantial

--- CleaningResponse.R
+++ CleaningResponse.R
@@ -1,39 +1,52 @@
 library(tidyverse)
+MSAlist <- read_csv("inputs/MSA list.csv")
 
-responserates <-  read.delim("Inputs/participationrates2010.txt", sep = "|")
-
-countyresponse <- responserates %>% 
+responserates <-  read.delim("Inputs/participationrates2010.txt", sep = "|") %>% 
+  select(-X, -X.1, -X.2, -X.3) %>%
   rename(Geography = "American.Indian.Area..Reservation.") %>% 
-  rename(County = "Big.Lagoon") %>% 
-  rename(GEOID5 = "X0240") %>% 
-  rename(Countyresponse2000 = "X0.60") %>% 
-  rename(Countyresponse2010 = "X0.00") %>% 
+  rename(Name = "Big.Lagoon") %>% 
+  rename(GEOID = "X0240") %>% 
+  rename(response2000 = "X0.60") %>% 
+  rename(response2010 = "X0.00")
+
+countyresponse <- responserates  %>% 
   filter(Geography == "County ") %>% 
   mutate(County = as.character(County),
-         GEOID5 = as.character(GEOID5)) %>% 
-  filter(str_detect(County, ", LA")) %>%
-  select(GEOID5, County, Geography, Countyresponse2000, Countyresponse2010)
-
-
+         GEOID = as.character(GEOID)) %>% 
+  filter(str_detect(County, ", LA"))
 
 tractresponse <- responserates %>% 
-  rename(Geography = "American.Indian.Area..Reservation.") %>% 
-  rename(County = "Big.Lagoon") %>% 
-  rename(GEOID11 = "X0240") %>% 
-  rename(response2000 = "X0.60") %>% 
-  rename(response2010 = "X0.00") %>% 
   filter(Geography == "Tract ") %>% 
-  mutate(GEOID11 = as.character(GEOID11)) %>% 
-  mutate(GEOID2 = substring(GEOID11, 1, 2),
+  mutate(GEOID = as.character(GEOID)) %>% 
+  mutate(GEOID2 = substring(GEOID, 1, 2),
          Geography = as.character(Geography)) %>%  
   filter(GEOID2 == "22") %>% 
-  select(GEOID11, GEOID2, Geography, response2000, response2010)  
+  select(-Name)  
+
+# The census defines cities according to their legally-defined boundaries. These are not metro areas. 
+cityresponse <- responserates %>%
+  filter(grepl("city, LA", Name))
+
+# For the same reason we can't aggregate tract-level rates up to neighborhoods, we cannot aggregate parish-level rates up to metros. This data table labels each parish with the metro to which it belongs. 
+metroresponse <-  countyresponse %>%
+  mutate(FIPS_5 = as.numeric(GEOID)) %>%
+  left_join(MSAlist,by = "FIPS_5") %>%
+  filter(`Metropolitan/Micropolitan Statistical Area` == "Metropolitan Statistical Area") %>%
+  select(-(8:13)) %>%
+  rename(Metro = "CBSA Title")
+
 
 countyresponse %>% 
 write.csv("countyresponse.csv")
 
 tractresponse %>% 
   write.csv("tractresponse.csv")
+
+cityresponse %>% 
+  write.csv("cityresponse.csv")
+
+metroresponse %>% 
+  write.csv("metroresponse.csv")
   
   
   
