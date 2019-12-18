777eb2a768b30263783cbc7f0e46fee5241fbfec
jennal-datacenter <42979143+jennal-datacenter@users.noreply.github.com>
2019-12-18 17:47:20 -0600

    cleaned up language pull

--- data pull.R
+++ data pull.R
@@ -40,18 +40,45 @@ age <- ageRaw %>%
   mutate(t4less = (m_under5 + f_under5), 
          t4lessmoeagg = sqrt(m_under5MOE^2 + f_under5MOE^2),
          t4lessCV = (t4lessmoeagg/1.64)/t4less)
+##calculates MOE for aggregated estimates
+##moe = sqrt(sum(estimateMOE^2))
+##input: dataframe of estimates' MOEs (i.e. use cbind)
+##output: column of MOEs
+moeagg <- function(estimateMOE){  
+  squares <- matrix(0, dim(estimateMOE)[1], dim(estimateMOE)[2])
+  for(i in 1:dim(estimateMOE)[2]){
+    squares[,i] <- t(estimateMOE[,i]*estimateMOE[,i])
+  }
+  sumsquares <- apply(squares, 1, sum)
+  return(sqrt(sumsquares))
+}
 
+##calculates MOE for proportions
+##p = x/y
+##moe = sqrt(moex^2 - p^2 * moey^2)/y
+##for ratio, +
+##input: columns of measures y, moex, moey, p
+##output: column of MOEs
+moeprop <- function(y, moex, moey, p, ratio = FALSE){
+  mp <- matrix(0, length(y))
+  for(i in 1:length(y)){
+    ifelse(is.nan(y[i])|is.nan(p[i]),
+           mp[i] <- NaN,
+           if((moex[i]*moex[i] - p[i]*p[i]*moey[i]*moey[i]) < 0 | ratio == TRUE){
+             mp[i] <- (sqrt(moex[i]*moex[i] + p[i]*p[i]*moey[i]*moey[i]))/y[i]
+           } else {
+             mp[i] <- (sqrt(moex[i]*moex[i] - p[i]*p[i]*moey[i]*moey[i]))/y[i]
+           })
+  }
+  return(mp)
+}
 
 # The most straighforward measure of English ability is probably the English ability question from the ACS.
 # 
 # A paper published at the Census Bureau reports "that the English-ability question, despite being a self-assessment, does a good job of measuring English ability."
 # https://www.census.gov/newsroom/blogs/research-matters/2015/10/how-well-do-you-speak-english-assessing-the-validity-of-the-american-community-survey-english-ability-question.html
 
-# In neighborhood profiles, we group together people who speak english "well" or "very well" and people who speak English "not well" or "not at all." The Census Bureau/LEP.gov considers anyone who speaks English less than "very well" to have limited English proficiency. 
-#
-# https://www.lep.gov/faqs/faqs.html#OneQ1
-
-# I recommend that becaue "Limited English Proficiency" is a known term, we use that indicator rather than a more stringent "Low English Proficiency." The data may tell us to go with the lower threshold, though!
+# In neighborhood profiles, we group together people who speak english "well" or "very well" and people who speak English "not well" or "not at all," with the latter group representing people for whom English is a barrier.  
 
 
 lang.vars <- c("B16005_001E","B16005_003E","B16005_005E","B16005_006E","B16005_007E","B16005_008E","B16005_010E","B16005_011E","B16005_012E","B16005_013E","B16005_015E","B16005_016E","B16005_017E","B16005_018E","B16005_020E","B16005_021E","B16005_022E","B16005_023E","B16005_025E","B16005_027E",
@@ -66,70 +93,20 @@ lang.names <- c("tot","nat.onlyeng","nat.span.vwell","nat.span.well","nat.span.n
                 "forbor.asian.notatallMOE","forbor.other.vwellMOE","forbor.other.wellMOE","forbor.other.notwellMOE","forbor.other.notatallMOE")
 langRaw <- np.pull(variables = lang.vars, names = lang.names)
 
-langRaw[langRaw == -555555555] <- 0 
-langUSRaw[langUSRaw == -555555555] <- 0 
+langRaw[langRaw == -555555555] <- 0  
 
 lang <- langRaw %>% 
-  mutate(engvwellpct = (nat.onlyeng + 
-                       forbor.onlyeng + 
-                       nat.euro.vwell + 
-                       nat.span.vwell + 
-                       #nat.span.well + 
-                       #forbor.span.well +   
-                       forbor.span.vwell + 
-                       #nat.euro.well + 
-                       nat.asian.vwell + 
-                       #nat.asian.well + 
-                       nat.other.vwell + 
-                       #nat.other.well + 
-                       forbor.euro.vwell + 
-                       #forbor.euro.well + 
-                       forbor.asian.vwell + 
-                       #forbor.asian.well + 
-                       #forbor.other.well + 
-                       forbor.other.vwell) / tot,
-         engwellpct = (nat.span.well + 
-                         forbor.span.well +                         
-                         nat.euro.well + 
-                         nat.asian.well + 
-                         nat.other.well +
-                         forbor.euro.well +
-                         forbor.asian.well + 
-                         forbor.other.well) / tot,)
-         engnotwellpct = (nat.span.notwell + 
-                          nat.span.notatall + 
-                          forbor.span.notwell + 
-                          forbor.span.notatall +
-                          nat.euro.notwell + 
-                          nat.euro.notatall + 
-                          nat.asian.notwell + 
-                          nat.asian.notatall + 
-                          nat.other.notwell + 
-                          nat.other.notatall + 
-                          forbor.euro.notwell + 
-                          forbor.euro.notatall + 
-                          forbor.other.notwell + 
-                          forbor.asian.notwell + 
-                          forbor.asian.notatall) / tot,
+  mutate(engwelltot = (nat.onlyeng + forbor.onlyeng + nat.euro.vwell + nat.span.vwell + nat.span.well + forbor.span.vwell + forbor.span.well + nat.euro.well + nat.asian.vwell + nat.asian.well + nat.other.vwell + nat.other.well + forbor.euro.vwell + forbor.euro.well + forbor.asian.vwell + forbor.asian.well + forbor.other.vwell + forbor.other.well),
+         engnotwelltot = (nat.span.notwell + nat.span.notatall + forbor.span.notwell + forbor.span.notatall + nat.euro.notwell + nat.euro.notatall + nat.asian.notwell + nat.asian.notatall + nat.other.notwell + nat.other.notatall + forbor.euro.notwell + forbor.euro.notatall + forbor.other.notwell + forbor.asian.notwell + forbor.asian.notatall),
+         
+         engwellpct = (nat.onlyeng + forbor.onlyeng + nat.euro.vwell + nat.span.vwell + nat.span.well + forbor.span.vwell + forbor.span.well + nat.euro.well + nat.asian.vwell + nat.asian.well + nat.other.vwell + nat.other.well + forbor.euro.vwell + forbor.euro.well + forbor.asian.vwell + forbor.asian.well + forbor.other.vwell + forbor.other.well) / tot,
+         engnotwellpct = (nat.span.notwell + nat.span.notatall + forbor.span.notwell + forbor.span.notatall + nat.euro.notwell + nat.euro.notatall + nat.asian.notwell + nat.asian.notatall + nat.other.notwell + nat.other.notatall + forbor.euro.notwell + forbor.euro.notatall + forbor.other.notwell + forbor.asian.notwell + forbor.asian.notatall) / tot,
          
          engwellMOE = moeagg(cbind(nat.onlyengMOE, forbor.onlyengMOE, nat.euro.vwellMOE, nat.span.vwellMOE, nat.span.wellMOE, forbor.span.vwellMOE, forbor.span.wellMOE, nat.euro.wellMOE, nat.asian.vwellMOE, nat.asian.wellMOE, nat.other.vwellMOE, nat.other.wellMOE, forbor.euro.vwellMOE, forbor.euro.wellMOE, forbor.asian.vwellMOE, forbor.asian.wellMOE, forbor.other.vwellMOE, forbor.other.wellMOE)),
-         spanengnotwellMOE = moeagg(cbind(nat.span.notwellMOE, nat.span.notatallMOE, forbor.span.notwellMOE, forbor.span.notatallMOE)),
-         engnotwellMOE = moeagg(cbind(nat.euro.notwellMOE, nat.euro.notatallMOE, nat.asian.notwellMOE, nat.asian.notatallMOE, nat.other.notwellMOE, nat.other.notatallMOE, forbor.euro.notwellMOE, forbor.euro.notatallMOE, forbor.other.notwellMOE, forbor.asian.notwellMOE, forbor.asian.notatallMOE)),
+         engnotwellMOE = moeagg(cbind(nat.span.notwellMOE, nat.span.notatallMOE, forbor.span.notwellMOE, forbor.span.notatallMOE,nat.euro.notwellMOE, nat.euro.notatallMOE, nat.asian.notwellMOE, nat.asian.notatallMOE, nat.other.notwellMOE, nat.other.notatallMOE, forbor.euro.notwellMOE, forbor.euro.notatallMOE, forbor.other.notwellMOE, forbor.asian.notwellMOE, forbor.asian.notatallMOE)),
          
          engwellMOEprop = moeprop(y= tot, moex = engwellMOE, moey = totMOE, p = engwellpct),
-         spanengnotwellMOEprop = moeprop(y= tot, moex = spanengnotwellMOE, moey = totMOE, p = spanengnotwellpct),
-         engnotwellMOEprop = moeprop(y= tot, moex = engnotwellMOE, moey = totMOE, p = engnotwellpct)
-  )
+         engnotwellMOEprop = moeprop(y= tot, moex = engnotwellMOE, moey = totMOE, p = engnotwellpct)) %>%
+  select(parish, tract, engwelltot, engnotwelltot, engwellpct, engnotwellpct, engwellMOE, engnotwellMOE, engwellMOEprop, engnotwellMOEprop)
 
-langEST2 <- langAnalysis %>% 
-  select(geo,contains("pct")) %>% 
-  mutate_at(vars(-1), funs(as.numeric)) %>% 
-  mutate_at(vars(-1), funs(scales::percent(.,accuracy = .1))) %>% 
-  gather(meas, val, -1)
-langMOE2 <- langAnalysis %>% 
-  select(geo, contains("MOEprop")) %>% 
-  mutate_at(vars(-1),funs(as.numeric)) %>% 
-  mutate_at(vars(-1), funs(scales::percent(.,accuracy = .1))) %>% 
-  gather(measMOE, valMOE, -1)
-llang <- bind_cols(langEST2, langMOE2) %>% select(-geo1)
 
