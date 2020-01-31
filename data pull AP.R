b808c22485cca2eda5b5b07e7b393b426440a051
Rachel Weinstein <rachelw@datacenterresearch.org>
2020-01-31 10:17:55 -0600

    for allison. complete

--- data pull AP.R
+++ data pull AP.R
@@ -59,10 +59,11 @@ stattest <- function(x, moex = matrix(0, length(x)), y, moey){
 }
 
 
+
 ## 2017
-race.vars <-c("B03002_001E","B03002_003E","B03002_004E","B03002_005E","B03002_006E","B03002_007E","B03002_008E","B03002_009E","B03002_012E","B03002_001M","B03002_003M","B03002_004M","B03002_005M","B03002_006M","B03002_007M","B03002_008M","B03002_009M","B03002_012M")
-race.names <-c("pop2017", "white2017","black2017","native2017","asian2017","island2017","other2017","two2017","hisp2017","popMOE", "whiteMOE","blackMOE","nativeMOE","asianMOE","islandMOE","otherMOE","twoMOE","hispMOE")
-raceRaw <- np.pull.parish(variables=race.vars, names=race.names) 
+#race.vars <-c("B03002_001E","B03002_003E","B03002_004E","B03002_005E","B03002_006E","B03002_007E","B03002_008E","B03002_009E","B03002_012E","B03002_001M","B03002_003M","B03002_004M","B03002_005M","B03002_006M","B03002_007M","B03002_008M","B03002_009M","B03002_012M")
+#race.names <-c("pop2017", "white2017","black2017","native2017","asian2017","island2017","other2017","two2017","hisp2017","popMOE", "whiteMOE","blackMOE","nativeMOE","asianMOE","islandMOE","otherMOE","twoMOE","hispMOE")
+#raceRaw <- np.pull.parish(variables=race.vars, names=race.names) 
 #save(raceRaw, file = "inputs/raceRaw.RData")
 
 
@@ -217,8 +218,9 @@ nativetablepct <- racetablepct %>%
          MOE = percent(MOE))
 
 othertablepct <- racetablepct %>% 
-  mutate(Race = "2 race categories") %>% 
+  
   select(name, Race, otherpct2000, otherpct2010, otherpct2017, otherMOEprop2017, otherSIG) %>% 
+  mutate(Race = "Other") %>% 
   rename(y2000 = "otherpct2000",
          y2010 = "otherpct2010",
          y2017 = "otherpct2017",
@@ -230,7 +232,7 @@ othertablepct <- racetablepct %>%
          MOE = percent(MOE))
 
 twotablepct <- racetablepct %>% 
-  mutate(Race = "Hispanic (any race)") %>% 
+  mutate(Race = "2 race categories") %>% 
   select(name, Race, twopct2000, twopct2010, twopct2017, twoMOEprop2017, twoSIG)%>% 
   rename(y2000 = "twopct2000",
          y2010 = "twopct2010",
@@ -243,7 +245,7 @@ twotablepct <- racetablepct %>%
          MOE = percent(MOE))
 
 hisptablepct <- racetablepct %>% 
-  mutate(Race = "Other") %>% 
+  mutate(Race = "Hispanic (any race)") %>% 
   select(name, Race, hisppct2000, hisppct2010, hisppct2017, hispMOEprop2017, hispSIG)%>% 
   rename(y2000 = "hisppct2000",
          y2010 = "hisppct2010",
@@ -256,7 +258,11 @@ hisptablepct <- racetablepct %>%
          MOE = percent(MOE))
 
 
-poptable <- bind_rows(blacktablepct , whitetablepct , asiantablepct , nativetablepct , twotablepct , hisptablepct , othertablepct ) 
+poptable <- bind_rows(blacktablepct , whitetablepct , asiantablepct , nativetablepct , twotablepct , hisptablepct , othertablepct ) %>% 
+  select(name, Race, y2000, y2010) %>% 
+  merge(., tablepercent2018, by = c("name", "Race"))
+
+write.csv(poptable, "outputs/Parish_Demographics_Percent.csv")
 
 
 
@@ -361,4 +367,127 @@ hisptable <- race %>%
          MOE = comma(MOE))
 
 
-poptableCount <- bind_rows(blacktable , whitetable , asiantable , nativetable , twotable , hisptable , othertable ) 
+
+
+
+####### PEP PULL
+
+
+allparishesRaw <- pullDataPEP(charagegroupsVars, 
+                              api = "pep/charagegroups", 
+                              year = 2017, 
+                              counties = mycounties)
+save(allparishesRaw, file = "inputs/allparishesRaw.Rdata")
+
+
+raceCodeName <- 
+  c("Total",
+    "White alone",
+    "Black or African American alone",
+    "American Indian and Alaska Native alone",
+    "Asian alone",
+    "Native Hawaiian and Other Pacific Islander alone",
+    "Two or more races",
+    "White combo",
+    "Black or African American combo",
+    "American Indian and Alaska Native combo",
+    "Asian combo",
+    "Native Hawaiian and Other Pacific Islander combo")
+raceCode <- data.frame(RACE=as.character(0:11),raceCodeName)
+
+hispCodeName <- c("Total","Not Hispanic","Hispanic")
+hispCode <- data.frame(HISP=as.character(0:2),hispCodeName)
+
+
+popestVars <- c("POP","DATE_DESC","DATE_CODE", "GEONAME", "HISP", "RACE")
+popestVars2000 <- c("POP","DATE_DESC","DATE_", "GEONAME", "HISP")
+listCensusMetadata("pep/int_charagegroups", vintage = 2000, type = "variables")
+race2018Raw <- getCensus(name = "pep/charagegroups", # most recent
+                         vintage = 2018, 
+                         key = "530ce361defc2c476e5b5d5626d224d8354b9b9a", 
+                         vars = popestVars, 
+                         region = "county: *", 
+                         regionin = "state:22") %>% 
+  filter(DATE_DESC == "7/1/2018 population estimate")
+
+race2018 <- race2018Raw %>% 
+  left_join(raceCode) %>%
+  left_join(hispCode) %>%
+  mutate(place = GEONAME) %>% 
+  mutate(POP = as.numeric(POP), 
+         place = GEONAME,
+         place = ifelse(!is.na(county),
+                        str_sub(GEONAME, 1, nchar(GEONAME) - 18),
+                        GEONAME)) %>% 
+  merge(., county_crosswalk, by = "county") %>% 
+  select(name, DATE_DESC, hispCodeName,  raceCodeName, POP) %>% 
+  rename(hisp = hispCodeName,
+         race = raceCodeName, 
+         population = POP,
+         date = DATE_DESC) %>% 
+  #filter(hisp != "Hispanic" & race != "Total" ) %>% 
+  filter(!grepl("combo", race)) %>% 
+  mutate(raceSimple = "other2018", # make variable base on race alone that matches Who Lives races. 
+         raceSimple = ifelse(race == "Total" & hisp == "Total", "pop2018", raceSimple),
+         raceSimple = ifelse(race == "White alone" & hisp == "Not Hispanic", "white2018", raceSimple),
+         raceSimple = ifelse(race == "Black or African American alone" & hisp == "Not Hispanic", "black2018", raceSimple),
+         raceSimple = ifelse(race == "Asian alone" & hisp == "Not Hispanic", "asian2018", raceSimple),
+         raceSimple = ifelse(race == "American Indian and Alaska Native alone" & hisp == "Not Hispanic", "native2018", raceSimple),
+         raceSimple = ifelse(race == "Two or more races" & hisp == "Not Hispanic", "two2018", raceSimple),
+         raceSimple = ifelse(race == "Total" & hisp == "Hispanic", "hisp2018", raceSimple),
+         raceSimple = ifelse(hisp == "Hispanic" & race != "Total", "NA", raceSimple),
+         raceSimple = ifelse(hisp == "Total" & race != "Total", "NA", raceSimple),
+         raceSimple = ifelse(hisp == "Not Hispanic" & race == "Total", "NA", raceSimple)) %>% 
+  filter(raceSimple != "NA") %>% 
+  select(name, population, raceSimple) %>% 
+  group_by(name, raceSimple) %>% 
+  summarise_all(funs(sum)) %>% 
+  pivot_wider(., id_cols = name, values_from = population, names_from= raceSimple) %>% 
+  mutate(   
+    blackpct2018 = black2018 / pop2018,
+    whitepct2018 = white2018 / pop2018,
+    asianpct2018 = asian2018 / pop2018,
+    nativepct2018 = native2018 / pop2018,
+    twopct2018 = two2018 / pop2018,
+    hisppct2018 = hisp2018 / pop2018,
+    otherpct2018 = other2018 /pop2018)
+
+
+
+tablepercent2018 <-  race2018 %>% 
+  dplyr::rename("Black or African American" = "blackpct2018",
+                "White" = "whitepct2018",
+                "Asian" = "asianpct2018",
+                "American Indian" = "nativepct2018", 
+                "Hispanic (any race)" = "hisppct2018",
+                "Other" = "otherpct2018",
+                "2 race categories" = "twopct2018") %>% 
+  select(name, "Black or African American", "White", "Asian", "American Indian", "Hispanic (any race)",  "2 race categories", "Other") %>% 
+  pivot_longer(-name, names_to = "Race", values_to = "y2018") %>% 
+  mutate(y2018 = percent(y2018)) %>% 
+  rename("name" = "name")
+
+tablecount2018 <-  race2018 %>% 
+  dplyr::rename("Black or African American" = "black2018",
+                "White" = "white2018",
+                "Asian" = "asian2018",
+                "American Indian" = "native2018", 
+                "Hispanic (any race)" = "hisp2018",
+                "Other" = "other2018",
+                "2 race categories" = "two2018") %>% 
+  dplyr::select("name", "Black or African American", "White", "Asian", "American Indian", "Hispanic (any race)",  "2 race categories", "Other") %>% 
+  pivot_longer(-name, names_to = "Race", values_to = "y2018") %>% 
+  mutate(y2018 = comma(y2018)) %>% 
+  rename("name" = "name")
+
+
+
+
+
+
+
+poptableCount <- bind_rows(blacktable , whitetable , asiantable , nativetable , twotable , hisptable , othertable ) %>% 
+  select(name, Race, y2000, y2010) %>% 
+  merge(., tablecount2018, by = c("name", "Race"))
+
+write.csv(poptableCount, "outputs/Parish_Demographics_Count.csv")
